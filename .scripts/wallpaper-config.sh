c8baa2162b399b923481e98316e8e3307ee3239a
Zachary Dillabough <zackdillabough@gmail.com>
2018-08-15 21:55:13 -0700

    updated

--- .scripts/wallpaper-config.sh
+++ .scripts/wallpaper-config.sh
@@ -1,9 +1,20 @@
 #!/bin/zsh
-source /home/zack/.cache/wal
 
-wal -b 000000 -n -i "$1"
-if [ "$1" = "/home/zack/Pictures/Backgrounds/Space" ]
-then
+bgpath="home/zack/Pictures/Backgrounds/"
+
+if [[ $1 == "" ]]; then
+	bgfldr="/home/zack/Pictures/Backgrounds/Circos"
+else
+	bgfldr="/home/zack/Pictures/Backgrounds/$1"
+fi
+
+# fin wallpaper var append to bg path
+
+wal -b 000000 -n -i $bgfldr
+#wal -b 000000 -i $bgfldr
+#source /home/zack/.cache/wal/colors.sh
+
+if [[ $1 == 'Space' ]]; then
 	feh --bg-scale "$(< "${HOME}/.cache/wal/wal")"
 else
 	feh --bg-center "$(< "${HOME}/.cache/wal/wal")"
