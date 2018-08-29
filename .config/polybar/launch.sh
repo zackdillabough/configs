20ed05b8e73cf17cd148bd9ae34b7cc994fcfaa6
Zack Dillabough <zackdillabough@gmail.com>
2018-08-28 22:18:48 -0700

    added polybar launch script for dual monitor setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,9 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 #polybar example
-polybar top &
-polybar bottom &
+polybar topleft &
+polybar topright &
+polybar bottomleft &
+polybar bottomright &
 
 echo "Bars launched..."
